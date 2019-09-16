2e18696644872e393f6243362c57db4802824809
darkwolfultima <35237705+darkwolfultima@users.noreply.github.com>
2019-09-16 12:08:50 -0400

    Update cpuminer-config.h

--- compat/cpuminer-config.h
+++ compat/cpuminer-config.h
@@ -85,25 +85,25 @@
 /* #undef NO_MINUS_C_MINUS_O */
 
 /* Name of package */
-#define PACKAGE "cpuminer-multi"
+#define PACKAGE "veriumMiner"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "cpuminer-multi"
+#define PACKAGE_NAME "veriumMiner"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "cpuminer-multi 1.3"
+#define PACKAGE_STRING "veriumMiner 1.4"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "cpuminer-multi"
+#define PACKAGE_TARNAME "veriumMiner"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.3"
+#define PACKAGE_VERSION "1.4"
 
 /* If using the C implementation of alloca, define if you know the
    direction of stack growth for your system; otherwise it will be
@@ -132,7 +132,7 @@
 #define USE_XOP 1
 
 /* Version number of package */
-#define VERSION "1.3"
+#define VERSION "1.4"
 
 /* Define to `unsigned int' if <sys/types.h> does not define. */
 /* #undef size_t */
